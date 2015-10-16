5635721dfac5c76f702c6ae1589004a4bc1489fa
Ramakrishna Kondapavuluru <ramakrishna1233@gmail.com>
2015-10-16 17:56:36 +1100

    bug 1
    add comments

--- src/Player.java
+++ src/Player.java
@@ -40,7 +40,7 @@ public class Player {
 		if (winnings < 0) throw new IllegalArgumentException("Winnings cannot be negative.");
 		balance = balance + winnings;		
 	}
-	
+	//bug one fix
 	public String toString() {
 		return String.format("Player: %s, Balance: %d, Limit: %d", name, balance, limit);
 	}
